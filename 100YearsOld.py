df90c0686ff93ddfdc93b6acda42353a7d3a7d6d
tobor <rosborne@osbornepro.com>
2019-08-02 15:26:33 -0600

    A few changes were made to spacing.

--- 100YearsOld.py
+++ 100YearsOld.py
@@ -1,15 +1,14 @@
 # When will you be 100 years old
-name = input("What is your name?: ") # Get users name
-agestr = input("How old are you? " )
+name = input("What is your name?: ")  # Get users name
+agestr = input("How old are you? ")
 
-while not agestr.isdigit(): # Loop until a number is entered
-    print('Please enter a number. Example 30 ')
-    agestr = input("How old are you? " )
+while not agestr.isdigit():  # Loop until a number is entered
+    print('Please enter a number. Example 30 ')'
+    agestr = input("How old are you? ")
 
 if agestr.isdigit():
     age = int(agestr)
 
 years = 100 - age
 
-print("In " + str(years) + " years, " + name + " will be 100 years old.")    
-
+print("In " + str(years) + " years, " + name + " will be 100 years old.")
