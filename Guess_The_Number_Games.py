b2dbe789cf738aeaa3a2c62fc58d891fe84deaac
tobor <rosborne@osbornepro.com>
2019-09-26 09:05:23 -0600

    Update Guess_The_Number_Games.py

--- Guess_The_Number_Games.py
+++ Guess_The_Number_Games.py
@@ -1,63 +1,59 @@
 play = 'yes'
-while play is 'yes':
+while play == 'yes':
 
     answer = input('========================================\n|   What game would you like to play?  |\n========================================\n\ta:\t\'100 Guesses\'\n\tb:\t\'Guess the Number\'\n\n\tAnswer is case sensitive: a or b? \n')
 
-    if (answer is 'a'):
+    if (answer == 'a'):
 
-############################
-#    GAME: 100 Guesses!    #
-############################
+        # GAME: 100 Guesses!
 
-	    print('INSTRUCTIONS: A number will be selected at random. Guess the number until you get it right. \n\tYou will be given hints if you get the answer wrong.\n\n\tGOOD LUCK!')
+        print('INSTRUCTIONS: A number will be selected at random. Guess the number until you get it right. \n\tYou will be given hints if you get the answer wrong.\n\n\tGOOD LUCK!')
 
-	    import random
+        import random
 
-	    n = random.randint(1,99)
-	    user_guess = input('Pick a number between 0 and 100. Let\'s see how close you get! ')
+        n = random.randint(1,99)
+        user_guess = input('Pick a number between 0 and 100. Let\'s see how close you get! ')
 
-	    if user_guess.isdigit():
-	        guess = int(user_guess)
+        if user_guess.isdigit():
+            guess = int(user_guess)
 
-	    while n != guess:
-	        print
-	        if guess < n:
-	            print('\tYou guessed a little low. Try Again!\n\t')
-	            user_guess = input('Enter a number between 0 and 100: ')
-	            if user_guess.isdigit():
-	                guess = int(user_guess)
-	            else:
-	                print('Enter a number. Example: 98')
-	        elif guess > n:
-	            print('\tYou guessed a little high. Try Again!\n\t')
-	            user_guess = input('Enter a number between 0 and 100: ')
-	            if user_guess.isdigit():
-	                guess = int(user_guess)
-	            else:
-	                print('Enter a number. Example: 89')
-	        else:
-	            break
+            while n != guess:
+                print
+                if guess < n:
+                    print('\tYou guessed a little low. Try Again!\n\t')
+                    user_guess = input('Enter a number between 0 and 100: ')
+                    if user_guess.isdigit():
+                        guess = int(user_guess)
+                    else:
+                        print('Enter a number. Example: 98')
+                    elif guess > n:
+                        print('\tYou guessed a little high. Try Again!\n\t')
+                        user_guess = input('Enter a number between 0 and 100: ')
+                        if user_guess.isdigit():
+                            guess = int(user_guess)
+                        else:
+                            print('Enter a number. Example: 89')
+                        else:
+                            break
+                            print('\nCONGRATULATIONS YOU WON!!! \n\nThe number was', n)
 
-	    print('\nCONGRATULATIONS YOU WON!!! \n\nThe number was', n)
+                            import turtle
+                            STAR_SIZE = 100
 
-	    import turtle
-	    STAR_SIZE = 100
+                            EXPANSION = 1.2
+                            TRANSLATION = STAR_SIZE * EXPANSION / 4
+                            turtle.hideturtle()
+                            turtle.color("green")
+                            turtle.shape("triangle")
+                            turtle.turtlesize(STAR_SIZE * EXPANSION / 20)
 
-	    EXPANSION = 1.2
-	    TRANSLATION = STAR_SIZE * EXPANSION / 4
+                            for _ in range(5):
+                                turtle.right(72)
+                                turtle.forward(TRANSLATION)
+                                turtle.stamp()
+                                turtle.backward(TRANSLATION)
 
-	    turtle.hideturtle()
-	    turtle.color("green")
-	    turtle.shape("triangle")
-	    turtle.turtlesize(STAR_SIZE * EXPANSION / 20)
-
-	    for _ in range(5):
-	        turtle.right(72)
-	        turtle.forward(TRANSLATION)
-	        turtle.stamp()
-	        turtle.backward(TRANSLATION)
-
-    elif answer is 'b':
+                            elif answer is 'b':
 
 #############################
 #  GAME: Guess the Number!  #
@@ -65,13 +61,13 @@ while play is 'yes':
 
         print('INSTRUCTIONS: A number will be picked at random. Try to guess what it is!!!\n\nGOOD LUCK! :) ')
         import random
-    
+
         random_number = random.randint(1,11)
         player_guess = input('Pick a number between 1 and 10: \n')
-    
+
         if player_guess.isdigit():
             guess = int(player_guess)
-    
+
         while random_number != guess:
             random_number = random.randint(1,11)
 
