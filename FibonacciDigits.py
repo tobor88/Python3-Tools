b2241541eabdd36f56d3ab2f1c81ca0763f47c79
tobor <rosborne@osbornepro.com>
2020-01-06 21:25:41 -0700

    Update FibonacciDigits.py

--- FibonacciDigits.py
+++ FibonacciDigits.py
@@ -1,12 +1,25 @@
 # How many fibonnacit digits to print
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
+'''
+EXAMPLE: fib(10)
+'''
+
+
+'''
+# The below code is for python3.7 As you will see in the above code, Python3.8 made some awesome changes that allow this code to be shorteded
 a = int(input('How many Fibonacci digits do you want to see?: '))
 
 if a == 1:
     b = [1]
 
 elif a != 1:
-    b = [1, 1]
+    b = [1,1]
 
     for i in b:
         if len(b) < a:
@@ -14,3 +27,4 @@ elif a != 1:
             b.append(i)
 
 print(b)
+'''
