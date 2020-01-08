b6da2aa119aaf2a53a653b0c2748c6f0fe47eb53
tobor <rosborne@osbornepro.com>
2020-01-08 00:32:09 -0700

    Update porttest.py

--- porttest.py
+++ porttest.py
@@ -1,7 +1,21 @@
 import socket
 
 ip = str(input("Enter the ip: "))
+
+try:
+    socket.inet_aton(ip)
+except socket.error as msg:
+    print("A valid ipv4 address was not defined {}".format(msg)) # This may need to be changed to + str(msg) depending on your python3 version
+
 port = int(input("Enter the port: "))
+# Validate port value
+def test_port(n):
+    if n in range(1, 65535):
+        print()
+    else:
+        print("A valid port number was not specified")
+ 
+test_port(port)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
