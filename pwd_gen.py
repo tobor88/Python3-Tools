fac2c3b7c77f882d8c3e10a81ecad588b59511b9
tobor <rosborne@osbornepro.com>
2020-01-14 00:16:04 -0700

    Update pwd_gen.py

--- pwd_gen.py
+++ pwd_gen.py
@@ -7,13 +7,14 @@ def pwd_gen(n):
     chars = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(chars) for i in range(n))
 
+def validate_num(num):
+    if (num.isdigit()):
+        vn = int(num)
+        return vn 
+    else:
+        print("Value error occured. Enter a valid number. Example: 18")
 
 num = input("Enter a number to define the length of the password: ")
-
-if (num.isdigit()):
-    n = int(num)
-    password = pwd_gen(n)
-    print(password)
-else:
-    print("Value Error occured. Please enter a valid number next time. Example: 18")
-
+n = validate_num(num)
+password = pwd_gen(n)
+print(password)
