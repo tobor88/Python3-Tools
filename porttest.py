5655a7dcd9274465f8601f4cb5b04a4a08bedd9f
tobor <rosborne@osbornepro.com>
2020-01-14 10:30:39 -0700

    Scan one port instead of range with editable ability to scan a range

--- porttest.py
+++ porttest.py
@@ -1,8 +1,6 @@
+# -*- coding: utf-8 -*-
 import socket
 import sys
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = str(input("IPv4 address of target: "))
 
 
 # Ensure IPv4 address is valid
@@ -20,9 +18,6 @@ def valid_ip(ip):
         return all(u >= head for u in slice_ip)
 
 
-valid_ip(ip)
-
-
 # Connect to port
 def port_connect(ip, port):
     try:
@@ -32,11 +27,35 @@ def port_connect(ip, port):
         return None
 
 
-# Check port range
-for port in range(1, 10000):
-    value = port_connect(ip, port)
-    if value == None:
-        pass
-    else:
+# The below function can be used for testing ports in a range
+def check_ports():
+    for port in range(1, 10000):
+        # print("Testing port {}".format(port))
+        value = port_connect(ip, port)
+        if value is None:
+            pass
+        else:
+            print("Port {} is open".format(port))
+            sys.exit(1)
+
+
+# The below function is used for testing whether or not a single port is open.
+def check_port(port):
+    result = port_connect(ip, port)
+    if result is None:
+        print("Port {} is closed".format(port))  # Replace this with pass
+    else:                                        # when you scan a port range
         print("Port {} is open".format(port))
-        break
+        sys.exit(1)
+
+
+def main():
+    valid_ip(ip)
+    check_port(port)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = str(input("IPv4 address of target: "))
+port = int(input("Enter port number to test: "))
+
+main()
