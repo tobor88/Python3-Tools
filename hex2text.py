8d272571b907268d68e0d2519ee15b9e564bc9a1
tobor <rosborne@osbornepro.com>
2022-04-22 11:47:36 -0600

    Update hex2text.py

--- hex2text.py
+++ hex2text.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import getopt
+import sys
+import re
+import argparse
 
-def hex2text(n):
+
+
+usage="""Syntax: hex2num [-h] -v 
+OsbornePro hex2num v1.1 ( https://osbornepro.com )
+
+    DESCRIPTION: hex2num is a tool created to quickly convert hex values to numbers
+
+    USAGE: hex2num -v <hex value to convert> 
+    
+    OPTIONS:
+    -h : Displays the help information for the command.
+    -v : Set the hex value to convert to a number 
+        
+    EXAMPLES:
+    hex2num -v FF
+    # This example translates FF to 255
+          
+    cat /tmp/hex.lst | hex2num -v 
+    # This example converts a list of hex values to their number
+"""
+argumentList = sys.argv[1:]
+options = "hv:"
+long_options = ["help","value"]
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--value", help = "Enter the hex value you want to convert to a number")
+args = parser.parse_args()
+
+if args.value:
+    n = args.value
     print(bytearray.fromhex(n).decode())
-
-n = input("Enter the hexadecimal value you want to conver to text: ")
-
-hex2text(n)
+   
+else:
+    print(str(usage))
